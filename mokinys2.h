a9487034c39df0364669b015cd7a1ae92f69608f
Ignas420 <ignassarapinas@gmail.com>
2024-04-11 15:47:25 +0300

    pakeista klase su deque

--- mokinys2.h
+++ mokinys2.h
@@ -20,14 +20,54 @@ const char CRfv2[] = "naujas_failas.txt";
 const char CRfv3[] = "mokslinciai.txt";
 const char CRfv4[] = "nuskriaustieji.txt";
 
-struct Mokinys
-{
+class Mokinys {
+private:
     string vardas;
     string pavarde;
     deque<int> ND;
     int egzaminas;
     double VID;
     double MED;
+
+public:
+    // Constructor
+    Mokinys() : egzaminas(0), VID(0), MED(0) {}
+
+    ~Mokinys(){}
+
+    // Getter functions
+    string getVardas() const { return vardas; }
+    string getPavarde() const { return pavarde; }
+    deque<int> getND() const { return ND; }
+    int getEgzaminas() const { return egzaminas; }
+    double getVID() const { return VID; }
+    double getMED() const { return MED; }
+
+    // Setter functions
+    void setVardas(const string &name) { vardas = name; }
+    void setPavarde(const string &surname) { pavarde = surname; }
+    void addND(int nd) { ND.push_back(nd); }
+    void clearND() { ND.clear(); }
+    void setEgzaminas(int exam) { egzaminas = exam; }
+    void setVID(double vid) { VID = vid; }
+    void setMED(double med) { MED = med; }
+
+    // Utility functions
+    bool Patikrinimas(string kint);
+    void Vidurkis(deque<Mokinys> & A);
+    void Isvedimas(const deque<Mokinys> &A, int MOK_kiekis, string isvedimas);
+    void Isvedimas2(const deque<Mokinys> &A, int MOK_kiekis, string isvedimas);
+    static bool PagalVidurki(const Mokinys &a, const Mokinys &b);
+    static bool PagalMediana(const Mokinys &a, const Mokinys &b);
+    static bool PagalVarda(const Mokinys &a, const Mokinys &b);
+    static bool PagalPavarde(const Mokinys &a, const Mokinys &b);
+    void Rikiavimas(deque<Mokinys> &Mokslinciai, deque<Mokinys> &Nuskriaustieji, deque<int> &IrasuSk);
+    void GeneruotiFailus(deque<Mokinys> &Nuskriaustieji, deque<Mokinys> &Mokslinciai, deque<int> &IrasuSk, deque<Mokinys> &A);
+    void Skaitymas(deque<Mokinys> &Nuskriaustieji, deque<Mokinys> &Mokslinciai, deque<int> &IrasuSk, string failas, deque<Mokinys> &A, int &temp);
+    void StudentuRusiavimas(deque<Mokinys> &Nuskriaustieji, deque<Mokinys> &Mokslinciai, deque<Mokinys> &A, deque<int> &IrasuSk, string failas, int &temp);
+    void StudentuRusiavimas2(deque<Mokinys> &Nuskriaustieji, deque<Mokinys> &Mokslinciai, deque<Mokinys> &A, deque<int> &IrasuSk, string failas, int &temp);
+    void StudentuRusiavimas3(deque<Mokinys> &Nuskriaustieji, deque<Mokinys> &Mokslinciai, deque<Mokinys> &A, deque<int> &IrasuSk, string failas, int &temp);
 };
+
 
 #endif
