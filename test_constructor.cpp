a79227ac729419558f8e3721dd7ce5170cb48f8e
Ignas Sarapinas <ignas.sarapinas@mif.stud.vu.lt>
2024-05-03 22:48:26 +0300

    unit tests

---
+++ test_constructor.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include "catch_amalgamated.hpp"
+#include "mokinys.h"
+
+TEST_CASE("Test constructor") {
+    // Create a Mokinys object with some initial values
+    Mokinys mokinys("John", "Doe", {10, 8, 9}, 7, 8.5, 8.2);
+    // Check if the constructor sets the values correctly
+    REQUIRE(mokinys.getVardas() == "John");
+    REQUIRE(mokinys.getPavarde() == "Doe");
+    REQUIRE(mokinys.getND() == vector<int>{10, 8, 9});
+    REQUIRE(mokinys.getEgzaminas() == 7);
+    REQUIRE(mokinys.getVID() == 8.5);
+    REQUIRE(mokinys.getMED() == 8.2);
+}
---
+++ test_constructor1.cpp
@@ -0,0 +1,14 @@
+#include "catch_amalgamated.hpp"
+#include "mokinys1.h"
+
+TEST_CASE("Test constructor") {
+    // Create a Mokinys object with some initial values
+    Mokinys mokinys("John", "Doe", {10, 8, 9}, 7, 8.5, 8.2);
+    // Check if the constructor sets the values correctly
+    REQUIRE(mokinys.getVardas() == "John");
+    REQUIRE(mokinys.getPavarde() == "Doe");
+    REQUIRE(mokinys.getND() == list<int>{10, 8, 9});
+    REQUIRE(mokinys.getEgzaminas() == 7);
+    REQUIRE(mokinys.getVID() == 8.5);
+    REQUIRE(mokinys.getMED() == 8.2);
+}
